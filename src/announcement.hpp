c7ca483dfdba9c0a3f3573ef1dd1f3e055acd95c
mblehmann <matheus@qualquer.inf.ufrgs.br>
2015-12-16 13:08:43 -0200

    Announcement Packet
    Added the Announcement Packet. The processing of this packet is not
    implemented yet.

---
+++ src/announcement.hpp
@@ -0,0 +1,260 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
+/**
+ * Copyright (c) 2013-2015 Regents of the University of California.
+ *
+ * This file is part of ndn-cxx library (NDN C++ library with eXperimental eXtensions).
+ *
+ * ndn-cxx library is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later version.
+ *
+ * ndn-cxx library is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more details.
+ *
+ * You should have received copies of the GNU General Public License and GNU Lesser
+ * General Public License along with ndn-cxx, e.g., in COPYING.md file.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ *
+ * See AUTHORS.md for complete list of ndn-cxx authors and contributors.
+ *
+ * @author Matheus Lehmann <mblehmann@inf.ufrgs.br>
+ */
+
+#ifndef NDN_ANNOUNCEMENT_HPP
+#define NDN_ANNOUNCEMENT_HPP
+
+#include "common.hpp"
+
+#include "name.hpp"
+#include "util/time.hpp"
+#include "management/nfd-local-control-header.hpp"
+#include "tag-host.hpp"
+
+namespace ndn {
+
+/** @var const unspecified_duration_type DEFAULT_ANNOUNCEMENT_TIMEOUT;
+ *  @brief default value for InterestLifetime
+ */
+const time::milliseconds DEFAULT_ANNOUNCEMENT_TIMEOUT = time::milliseconds(4000);
+
+/** @brief represents an Announcement packet
+ */
+class Announcement : public TagHost, public enable_shared_from_this<Announcement>
+{
+public:
+  class Error : public tlv::Error
+  {
+  public:
+    explicit
+    Error(const std::string& what)
+      : tlv::Error(what)
+    {
+    }
+  };
+
+  /** @brief Create a new Announcement with the given name
+   *  @param name The name for the announced content.
+   *  @note This constructor allows implicit conversion from Name.
+   *  @warning In certain contexts that use Announcement::shared_from_this(), Announcement must be created
+   *           using `make_shared`. Otherwise, .shared_from_this() will throw an exception.
+   */
+  Announcement(const Name& name);
+
+  /** @brief Create a new Announcement with the given name and announcement timeout
+   *  @param name             The name for the announced content.
+   *  @param interestLifetime The announcement timeout in time::milliseconds, or -1 for none (persistent FIB entry).
+   *  @warning In certain contexts that use Announcement::shared_from_this(), Announcement must be created
+   *           using `make_shared`. Otherwise, .shared_from_this() will throw an exception.
+   */
+  Announcement(const Name& name, const time::milliseconds& interestLifetime);
+
+  /** @brief Create from wire encoding
+   *  @warning In certain contexts that use Announcement::shared_from_this(), Announcement must be created
+   *           using `make_shared`. Otherwise, .shared_from_this() will throw an exception.
+   */
+  explicit
+  Announcement(const Block& wire);
+
+  /**
+   * @brief Fast encoding or block size estimation
+   */
+  template<encoding::Tag TAG>
+  size_t
+  wireEncode(EncodingImpl<TAG>& encoder) const;
+
+  /**
+   * @brief Encode to a wire format
+   */
+  const Block&
+  wireEncode() const;
+
+  /**
+   * @brief Decode from the wire format
+   */
+  void
+  wireDecode(const Block& wire);
+
+  /**
+   * @brief Check if already has wire
+   */
+  bool
+  hasWire() const
+  {
+    return m_wire.hasWire();
+  }
+
+  /**
+   * @brief Encode the name according to the NDN URI Scheme
+   */
+  std::string
+  toUri() const;
+
+public: // matching
+  /** @brief Check if Announcement matches the given @p name
+   *  @param name The name to be matched
+   */
+  bool
+  matchesName(const Name& name) const;
+
+public: // Name and guiders
+  const Name&
+  getName() const
+  {
+    return m_name;
+  }
+
+  Announcement&
+  setName(const Name& name)
+  {
+    m_name = name;
+    m_wire.reset();
+    return *this;
+  }
+
+  const time::milliseconds&
+  getAnnouncementTimeout() const
+  {
+    return m_announcementTimeout;
+  }
+
+  Announcement&
+  setAnnouncementTimeout(const time::milliseconds& announcementTimeout)
+  {
+    m_announcementTimeout = announcementTimeout;
+    m_wire.reset();
+    return *this;
+  }
+
+  /** @brief Check if Nonce set
+   */
+  bool
+  hasNonce() const
+  {
+    return m_nonce.hasWire();
+  }
+
+  /** @brief Get Announcement's nonce
+   *
+   *  If nonce was not set before this call, it will be automatically assigned to a random value
+   */
+  uint32_t
+  getNonce() const;
+
+  /** @brief Set Announcement's nonce
+   *
+   *  If wire format already exists, this call simply replaces nonce in the
+   *  existing wire format, without resetting and recreating it.
+   */
+  Announcement&
+  setNonce(uint32_t nonce);
+
+  /** @brief Refresh nonce
+   *
+   *  It's guaranteed that new nonce value differs from the existing one.
+   *
+   *  If nonce is already set, it will be updated to a different random value.
+   *  If nonce is not set, this method does nothing.
+   */
+  void
+  refreshNonce();
+
+public: // local control header
+  nfd::LocalControlHeader&
+  getLocalControlHeader()
+  {
+    return m_localControlHeader;
+  }
+
+  const nfd::LocalControlHeader&
+  getLocalControlHeader() const
+  {
+    return m_localControlHeader;
+  }
+
+  uint64_t
+  getIncomingFaceId() const
+  {
+    return getLocalControlHeader().getIncomingFaceId();
+  }
+
+  Announcement&
+  setIncomingFaceId(uint64_t incomingFaceId)
+  {
+    getLocalControlHeader().setIncomingFaceId(incomingFaceId);
+    // ! do not reset Announcement's wire !
+    return *this;
+  }
+
+  uint64_t
+  getNextHopFaceId() const
+  {
+    return getLocalControlHeader().getNextHopFaceId();
+  }
+
+  Announcement&
+  setNextHopFaceId(uint64_t nextHopFaceId)
+  {
+    getLocalControlHeader().setNextHopFaceId(nextHopFaceId);
+    // ! do not reset Announcement's wire !
+    return *this;
+  }
+
+public: // EqualityComparable concept
+  bool
+  operator==(const Announcement& other) const
+  {
+    return wireEncode() == other.wireEncode();
+  }
+
+  bool
+  operator!=(const Announcement& other) const
+  {
+    return !(*this == other);
+  }
+
+private:
+  Name m_name;
+  mutable Block m_nonce;
+  time::milliseconds m_announcementTimeout;
+
+  mutable Block m_wire;
+
+  nfd::LocalControlHeader m_localControlHeader;
+  friend class nfd::LocalControlHeader;
+};
+
+std::ostream&
+operator<<(std::ostream& os, const Announcement& announcement);
+
+inline std::string
+Announcement::toUri() const
+{
+  std::ostringstream os;
+  os << *this;
+  return os.str();
+}
+
+} // namespace ndn
+
+#endif // NDN_ANNOUNCEMENT_HPP
